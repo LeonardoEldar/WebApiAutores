8ab5e45a32e155bc052914b32a19f12f18ef2df7
DESKTOP-00S3F0A\Leonardo Di Primo <leonardo.diprimo@eldars.com.ar>
2022-03-03 11:41:03 -0300

    Model Binding Author

--- WebApiAutores/Controllers/AuthorsController.cs
+++ WebApiAutores/Controllers/AuthorsController.cs
@@ -5,32 +5,72 @@ using WebApiAutores.Entities;
 namespace WebApiAutores.Controllers
 {
     [ApiController]
-    [Route("api/authors")]
+    // Fill the routing with the name of the controller
+    [Route("api/[controller]")]
     public class AuthorsController : ControllerBase
     {
 
         private readonly ApplicationDbContext context;
 
-        public AuthorsController(ApplicationDbContext context) {
+        public AuthorsController(ApplicationDbContext context)
+        {
             this.context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Author>>> findAll() {
+        public async Task<ActionResult<List<Author>>> findAll()
+        {
             return await context.Authors.Include(author => author.Books).ToListAsync();
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Author>> findById(int id)
+        {
+            Author author = await context.Authors.Include(author => author.Books)
+                .FirstOrDefaultAsync(author => author.Id == id);
+
+            if (author == null) return NotFound($"Author with Id {id} not found");
+
+            return author;
+        }
+
+
+        [HttpGet("{name}")]
+        public async Task<ActionResult<Author>> findByName([FromRoute] String name)
+        {
+            Author author = await context.Authors.Include(author => author.Books)
+                .FirstOrDefaultAsync(author => author.Name.ToUpper().Contains(name.ToUpper()));
+
+            if (author == null) return NotFound($"Author with name {name} not found");
+
+            return author;
+        }
+
+        [HttpGet("nameFromQuery")]
+        public async Task<ActionResult<Author>> findByNameFromQuery([FromQuery] String name)
+        {
+            Author author = await context.Authors.Include(author => author.Books)
+                .FirstOrDefaultAsync(author => author.Name.ToUpper().Contains(name.ToUpper()));
+
+            if (author == null) return NotFound($"Author with name {name} not found");
+
+            return author;
+        }
+
+
         [HttpPost]
-        public async Task<ActionResult> create(Author author) {
+        public async Task<ActionResult> create([FromBody] Author author)
+        {
             context.Add(author);
             await context.SaveChangesAsync();
             return Ok();
         }
 
         [HttpPut("{id:int}")]
-        public async Task<ActionResult> update(Author author, int id) {
+        public async Task<ActionResult> update(Author author, int id)
+        {
             if (author.Id != id) return BadRequest("Author Id does not match URL Id");
-            
+
             bool exists = await context.Authors.AnyAsync(author => author.Id == id);
             if (!exists) return NotFound();
 
@@ -40,7 +80,8 @@ namespace WebApiAutores.Controllers
         }
 
         [HttpDelete("{id:int}")]
-        public async Task<ActionResult> delete(int id) {
+        public async Task<ActionResult> delete(int id)
+        {
             bool exists = await context.Authors.AnyAsync(author => author.Id == id);
             if (!exists) return NotFound();
 
